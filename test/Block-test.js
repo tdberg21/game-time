05e6e0bc9ff3cec87a5ccda090d5e5099a575cd1
Tory Dannenberg <rfdberg@gmail.com>
2018-05-02 15:00:40 -0600

    Added classes to files

--- test/Block-test.js
+++ test/Block-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-describe('Box', function () {
-  it('should return true', function () {
-    assert.equal(true, true);
+describe('Block', function () {
+  it('should be a function', function () {
+    assert.isFunction(Block);
   });
 });
