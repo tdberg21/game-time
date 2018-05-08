8644b3e9d2d20b85ccd4376f76e997fc302453f2
Megan Kaiser <meganrayanne@yahoo.com>
2018-05-08 10:07:26 -0600

    Add more tests on game file, highscore, growth

--- test/Game-test.js
+++ test/Game-test.js
@@ -6,7 +6,7 @@ const Game = require('../lib/Game.js')
 describe('Game', () => {
 
   let game;
-  
+
   global.window = {}
   window.localStorage = global.localStorage;
 
@@ -14,17 +14,54 @@ describe('Game', () => {
     game = new Game();
   })
 
-  it('should have default values', () => { 
+  it('should have default values', () => {
     assert.equal(game.direction, 'right');
     assert.equal(game.stopGame, false);
     assert.equal(game.lives, 3);
     assert.equal(game.score, 0);
   });
 
+  it('should keep the snake moving', () => {
+    game.gameSnake.head.x = 100;
+    game.keepSnakeMoving();
+
+    assert.equal(game.gameSnake.tail.x, 110)
+  });
+
   it('should lose a life if the snake dies', () => {
     game.gameSnake.die = true;
     game.newLife();
-    assert.equal(game.lives, 2)
+
+    assert.equal(game.lives, 2);
+  });
+
+  it('if you lose a life the game resets to new snake going right', () => {
+    game.gameSnake.die = true;
+    game.newLife();
+
+    assert.equal(game.direction, 'right');
+  });
+
+  it('should increase the score when it eats an apple', () => {
+    game.gameSnake.head.x = 100;
+    game.currentFood.x = 100;
+    game.gameSnake.head.y = 100;
+    game.currentFood.y = 100;
+    game.score = 0;
+    game.eatFood();
+
+    assert.equal(game.score, 1);
+  });
+
+  it('the snake should grow when it eats an apple', () => {
+    game.gameSnake.head.x = 100;
+    game.currentFood.x = 100;
+    game.gameSnake.head.y = 100;
+    game.currentFood.y = 100;
+    game.score = 0;
+    game.eatFood();
+
+    assert.equal(game.gameSnake.snakeBody.length, 5);
   });
 
   it('should get game over if your lives equal 0', () => {
@@ -36,15 +73,26 @@ describe('Game', () => {
     game.newLife();
 
     assert.equal(game.gameOver, true);
+    assert.equal(game.stopGame, true);
   });
-  
-  it('should increase the score when it eats an apple', () => {
-    game.gameSnake.head.x = 100;
-    game.currentFood.x = 100;
-    game.gameSnake.head.y = 100;
-    game.currentFood.y = 100;
-    game.score = 0;
-    game.eatFood();
-    assert.equal(game.score, 1)
+
+  it('should check for high score if you lose', () => {
+    game.highScore = 5;
+    for (let i = 0; i < 6; i++) {
+      game.gameSnake.head.x = 100;
+      game.currentFood.x = 100;
+      game.gameSnake.head.y = 100;
+      game.currentFood.y = 100;
+      game.eatFood();
+    }
+
+    game.gameSnake.die = true;
+    game.newLife();
+    game.gameSnake.die = true;
+    game.newLife();
+    game.gameSnake.die = true;
+    game.newLife();
+
+    assert.equal(game.highScore, 6);
   });
 });
